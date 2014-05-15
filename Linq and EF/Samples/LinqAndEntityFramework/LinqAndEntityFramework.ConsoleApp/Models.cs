05e4457a9259a997813bdfd69f0efb8c8274fa2a
Foyzul Karim <foyzulkarim@gmail.com>
2014-05-15 07:00:54 +0600

    initial commit

---
+++ Linq and EF/Samples/LinqAndEntityFramework/LinqAndEntityFramework.ConsoleApp/Models.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndEntityFramework.ConsoleApp
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Category> Categories { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Group Group { get; set; }
+        public List<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class DataProvider
+    {
+        public static List<Product> GetProducts()
+        {
+            Group drinks = new Group() { Id = 1, Name = "Drinks" };
+            Category coldDrinks = new Category() { Id = 1, Name = "Cold Drinks", Group = drinks };
+            Category hotDrinks = new Category(){Id = 2, Name = "Hot Drinks", Group = drinks};
+            Product coke = new Product() { Id = 1, Name = "Coke", Code = "101", Price = 30.0, Category = coldDrinks };
+            Product lassi = new Product(){Id = 2,Name = "Lassi", Code = "102", Price = 40.0, Category = coldDrinks};            
+            Product coffee = new Product() { Id = 3, Name = "Coffee", Code = "103", Price = 60.0, Category = hotDrinks };
+            
+
+            List<Product> products = new List<Product>();
+            products.Add(lassi);
+            products.Add(coke);
+            products.Add(coffee);
+
+            return products;
+        }
+    }
+}
---
+++ Linq and EF/Samples/LinqAndEntityFramework/LinqAndEntityFramework.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndEntityFramework.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = DataProvider.GetProducts();
+            foreach (Product product in products)
+            {
+                Console.WriteLine(string.Format("Product Name : {0}, Price : {1}, Category : {2}, Group : {3}",
+                    product.Name, product.Price, product.Category.Name, product.Category.Group.Name));
+
+            }
+            
+
+            Console.Read();
+        }
+    }
+
+}
