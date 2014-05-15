429a2522fdff3dadfc937fe2f892531380feddaf
IbrahimIslam <dev.ibrahim.islam@gmail.com>
2014-05-15 15:21:52 +0600

    Added dummy data for all lists

--- Linq and EF/Samples/LinqAndEntityFramework/LinqAndEntityFramework.ConsoleApp/Models.cs
+++ Linq and EF/Samples/LinqAndEntityFramework/LinqAndEntityFramework.ConsoleApp/Models.cs
@@ -79,7 +79,15 @@ namespace LinqAndEntityFramework.ConsoleApp
         {
             var categories = new List<Category> { 
                 new Category{ Id = 1, Group = GetGroup(1), Name = "Hot Drinks" },
-                new Category{ Id = 2, Group = GetGroup(1), Name = "Cold Drinks" }
+                new Category{ Id = 2, Group = GetGroup(1), Name = "Cold Drinks" },
+                new Category{ Id = 3, Group = GetGroup(2), Name = "Chinese" },
+                new Category{ Id = 4, Group = GetGroup(2), Name = "Indian" },                
+                new Category{ Id = 5, Group = GetGroup(3), Name = "Desert" },
+                new Category{ Id = 6, Group = GetGroup(4), Name = "Appetizers" },
+                new Category{ Id = 7, Group = GetGroup(5), Name = "Long Grain" },
+                new Category{ Id = 8, Group = GetGroup(5), Name = "Short Grain" },
+                new Category{ Id = 9, Group = GetGroup(6), Name = "Wheat" },
+                new Category{ Id = 10, Group = GetGroup(6), Name = "Corn" }
             };
 
             Category category = new Category();
@@ -96,27 +104,43 @@ namespace LinqAndEntityFramework.ConsoleApp
 
         private static List<Product> GetBreads()
         {
-            return new List<Product>();
+            return new List<Product> { 
+                new Product{ Id = 4, Name = "Shaan Bread", Code = "104", Price = 35.00, Category = GetCategory(9)  },
+                new Product{ Id = 5, Name = "Well Foods", Code = "105", Price = 120.00, Category = GetCategory(9)  },
+                new Product{ Id = 6, Name = "Cooper's", Code = "106", Price = 250.00, Category = GetCategory(10)  }
+            };
         }
 
         private static List<Product> GetRices()
         {
-            return new List<Product>();
+            return new List<Product> { 
+                new Product{ Id = 7, Name = "Basmati", Code = "107", Price = 300, Category = GetCategory(7) },
+                new Product{ Id = 8, Name = "Minicat", Code = "108", Price = 280, Category = GetCategory(8) }
+            };
         }
 
         private static List<Product> GetStarters()
         {
-            return new List<Product>();
+            return new List<Product> { 
+                new Product{ Id = 9, Name = "Unthun", Code = "109", Price = 80, Category = GetCategory(6) }
+            };
         }
 
         private static List<Product> GetSideDishes()
         {
-            return new List<Product>();
+            return new List<Product> {
+                new Product{ Id = 10, Name = "Ice Cream", Code = "110", Price = 90, Category = GetCategory(5) },
+                new Product{ Id = 11, Name = "Sweets", Code = "111", Price = 150, Category = GetCategory(5) }
+            };
         }
 
         private static List<Product> GetMainDishes()
         {
-            return new List<Product>();
+            return new List<Product> { 
+                new Product{ Id = 12, Name = "Fried Rice and Chicken", Code = "112", Price = 180, Category = GetCategory(3) },
+                new Product{ Id = 13, Name = "Chola Batora", Code = "113", Price = 150, Category = GetCategory(4) },
+                new Product{ Id = 14, Name = "Dosa", Code = "114", Price = 140, Category = GetCategory(4) }
+            };
         }
 
         private static List<Product> GetDrinks()
