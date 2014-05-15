ec3fefb58670144d54266e1026bb34d9729a217c
foyzul <foyzul@outlook.com>
2014-05-15 07:12:31 +0600

    Update Program.cs

--- Linq and EF/Samples/LinqAndEntityFramework/LinqAndEntityFramework.ConsoleApp/Program.cs
+++ Linq and EF/Samples/LinqAndEntityFramework/LinqAndEntityFramework.ConsoleApp/Program.cs
@@ -15,10 +15,8 @@ namespace LinqAndEntityFramework.ConsoleApp
             {
                 Console.WriteLine(string.Format("Product Name : {0}, Price : {1}, Category : {2}, Group : {3}",
                     product.Name, product.Price, product.Category.Name, product.Category.Group.Name));
-
             }
             
-
             Console.Read();
         }
     }
