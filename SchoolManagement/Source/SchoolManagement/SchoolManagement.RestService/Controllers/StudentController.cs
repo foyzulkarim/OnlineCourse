07733943f2ac64c4d9c8e60b9fa973153d95d9ab
Foyzul Karim <foyzul@outlook.com>
2014-05-27 10:40:30 +0600

    day 5

---
+++ SchoolManagement/Source/SchoolManagement/SchoolManagement.RestService/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SchoolManagement.Manager;
+using SchoolManagement.Model;
+
+namespace SchoolManagement.RestService.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public List<Student> Get()
+        {
+            StudentManager manager = new StudentManager();
+            List<Student> students = manager.GetStudents();
+            return students;
+        }
+    }
+}
---
+++ SchoolManagement/Source/SchoolManagement/SchoolManagement.RestService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolManagement.RestService.WebApiApplication" Language="C#" %>
