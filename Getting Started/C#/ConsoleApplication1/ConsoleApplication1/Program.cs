ad7a58936c9afd7aeac8cae2f6288c83eee41347
foyzulkarim <foyzulkarim@gmail.com>
2015-01-23 08:34:31 +0600

    Getting Started with C Sharp Part 1 Console Application

---
+++ Getting Started/C#/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string result = input1 + input2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
