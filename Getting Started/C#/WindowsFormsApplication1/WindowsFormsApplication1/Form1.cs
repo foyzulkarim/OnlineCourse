0fa223d7fb94d37b6c33630d9830d32489373cba
foyzulkarim <foyzulkarim@gmail.com>
2015-01-28 23:34:30 +0600

    Part 4, Desktop application

---
+++ Getting Started/C#/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // grab the value from the textbox
+            
+            // we should store it in a variable
+            string value = textBox1.Text;
+            // we will display it as a messagebox info
+            MessageBox.Show(value);
+        }
+    }
+}
