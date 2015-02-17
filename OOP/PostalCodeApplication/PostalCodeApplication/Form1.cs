a086ae2808645b08b04976e63c4d4713806bbb1b
foyzulkarim <foyzulkarim@gmail.com>
2015-02-18 01:31:53 +0600

    part 12 OOP and Gridview

---
+++ OOP/PostalCodeApplication/PostalCodeApplication/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostalCodeApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // read from notepad
+            StreamReader reader = new StreamReader("postal codes.txt");
+            string line = reader.ReadLine();
+            List<PostalCode> postalCodes = new List<PostalCode>();
+           
+            while (line!=null)
+            {
+                string[] strings = line.Split(new[] { '\t' });
+                PostalCode p1 = new PostalCode();
+                p1.District = strings[0];
+                p1.PoliceStation = strings[1];
+                p1.SubOffice = strings[2];
+                p1.PostCode = strings[3];
+                postalCodes.Add(p1);
+                line = reader.ReadLine();
+            }
+ 
+            dataGridView1.DataSource = postalCodes;
+            // show in gridview
+        }
+    }
+}
---
+++ OOP/PostalCodeApplication/PostalCodeApplication/PostalCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalCodeApplication
+{
+    public class PostalCode
+    {
+        public string District { get; set; }
+        public string PoliceStation { get; set; }
+        public string SubOffice { get; set; }
+        public string PostCode { get; set; }
+    }
+}
