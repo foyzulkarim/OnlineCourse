050772524a8cc9bdd970581b8c30b5f803ad0b08
foyzulkarim <foyzulkarim@gmail.com>
2015-02-20 23:11:55 +0600

    Part 13: Linq dynamic searching

--- OOP/PostalCodeApplication/PostalCodeApplication/Form1.Designer.cs
+++ OOP/PostalCodeApplication/PostalCodeApplication/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,30 +38,41 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 53);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(880, 651);
+            this.dataGridView1.Size = new System.Drawing.Size(880, 598);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(880, 651);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- OOP/PostalCodeApplication/PostalCodeApplication/Form1.cs
+++ OOP/PostalCodeApplication/PostalCodeApplication/Form1.cs
@@ -13,17 +13,20 @@ namespace PostalCodeApplication
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        List<PostalCode> allPostalCodes = new List<PostalCode>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
             // read from notepad
             StreamReader reader = new StreamReader("postal codes.txt");
             string line = reader.ReadLine();
-            List<PostalCode> postalCodes = new List<PostalCode>();
+         
            
             while (line!=null)
             {
@@ -33,12 +36,28 @@ namespace PostalCodeApplication
                 p1.PoliceStation = strings[1];
                 p1.SubOffice = strings[2];
                 p1.PostCode = strings[3];
-                postalCodes.Add(p1);
+                allPostalCodes.Add(p1);
                 line = reader.ReadLine();
             }
  
-            dataGridView1.DataSource = postalCodes;
+            dataGridView1.DataSource = allPostalCodes;
             // show in gridview
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // we want to filter on the main data set
+            // get the input into a variable
+            string query = textBox1.Text.ToUpper();
+            // filter on the data set
+            var postalCodes = from code in allPostalCodes
+                where
+                    code.PoliceStation.ToUpper().Contains(query) || 
+                    code.SubOffice.ToUpper().Contains(query) ||
+                    code.PostCode.ToUpper().Contains(query)
+                select code;
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = postalCodes.ToList();
         }
     }
 }
