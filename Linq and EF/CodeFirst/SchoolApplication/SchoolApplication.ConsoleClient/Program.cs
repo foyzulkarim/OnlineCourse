eec75943d96c2703fc5f2f78fab0770b0f3bd2f7
foyzulkarim <foyzulkarim@gmail.com>
2015-02-28 07:45:28 +0600

    Part 14 : Code First initial commit

---
+++ Linq and EF/CodeFirst/SchoolApplication/SchoolApplication.ConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolApplication.DataAccess;
+using SchoolApplication.Model;
+
+namespace SchoolApplication.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolDataContext db = new SchoolDataContext();
+            var students = db.Students.ToList();
+            foreach (Student s in students)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Linq and EF/CodeFirst/SchoolApplication/SchoolApplication.DataAccess/SchoolDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolApplication.Model;
+
+namespace SchoolApplication.DataAccess
+{
+    public class SchoolDataContext : DbContext
+    {
+        public SchoolDataContext(): base("SchoolDb")
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
