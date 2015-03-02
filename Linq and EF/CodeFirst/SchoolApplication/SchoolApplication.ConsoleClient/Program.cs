5d14c0d1fddada5b9534c2bf65c4f9218e9c630c
foyzulkarim <foyzulkarim@gmail.com>
2015-03-02 10:09:16 +0600

    CRUD operation Code First

--- Linq and EF/CodeFirst/SchoolApplication/SchoolApplication.ConsoleClient/Program.cs
+++ Linq and EF/CodeFirst/SchoolApplication/SchoolApplication.ConsoleClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,37 @@ namespace SchoolApplication.ConsoleClient
     {
         static void Main(string[] args)
         {
-            SchoolDataContext db = new SchoolDataContext();
-            var students = db.Students.ToList();
+            using (SchoolDataContext db = new SchoolDataContext())
+            {
+                Console.WriteLine("Update a new student. ");
+                Student student = new Student();
+                Console.WriteLine("Write the Id");
+                student.StudentId = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine("Write name: ");
+                //student.Name = Console.ReadLine();
+                //Console.WriteLine("Write address: ");
+                //student.Address = Console.ReadLine();
+                //Console.WriteLine("Write phone: ");
+                //student.Phone = Console.ReadLine();
+               
+                db.Students.Attach(student);
+                db.Entry(student).State = EntityState.Deleted;
+                //db.Students.Remove(student);
+                db.SaveChanges();
+
+                DisplayStudents(db.Students.ToList());
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static void DisplayStudents(List<Student> students)
+        {
             foreach (Student s in students)
             {
                 Console.WriteLine(s.Name);
             }
-            Console.WriteLine("Done");
-            Console.ReadLine();
         }
     }
 }
